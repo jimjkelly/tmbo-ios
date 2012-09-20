d2641d3d294c135717362cdea17486805756c738
Scott Perry <github@numist.net>
2012-09-20 11:19:44 -0700

    Add a local token to TMBODataModelTests In such a way that no one should ever *ever* check in a token. Sure, they're easy to revoke, but this is still better.

---
+++ Frameworks/TMBODataModel/TMBODataModelTests/TMBODataModelToken.h
@@ -0,0 +1,16 @@
+//
+//  TMBODataModelToken.h
+//  TMBODataModel
+//
+//  Created by Scott Perry on 09/20/12.
+//  Copyright (c) 2012 Scott Perry. All rights reserved.
+//
+
+#include "TMBODataModelToken-local.h"
+
+#if 0
+
+// Copy the following line into $(PROJECT_DIR)/TMBODataModelTests/TMBODataModel-local.h:
+NSString * const kTMBOToken = <#token#>;
+
+#endif
