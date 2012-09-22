b2c71e801c94a6e5d25d9a7beef58b27abdc1799
Scott Perry <github@numist.net>
2012-09-21 17:47:01 -0700

    Smart auto-layout to maximise filename length
    Because the comments and votes labels are variable-width, too.

--- TMBO/Views/TMBOImageListCell.m
+++ TMBO/Views/TMBOImageListCell.m
@@ -16,12 +16,12 @@
 @synthesize votesView;
 @synthesize spinner;
 
+#define kXPadding 4.0
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
+    if (!self) return nil;
     return self;
 }
 
@@ -30,6 +30,62 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)layoutSubviews;
+{
+    CGSize cellSize = self.frame.size;
+    CGRect frame, thumbFrame = [self.thumbnailView frame];
+    
+    // votesView has height (as needed) and width (as needed) and anchors at (cellWidth - (votesViewWidth + padding), cellHeight - (votesViewHeight + padding)
+    UILabel *label = self.votesView;
+    CGSize size = [[label text] sizeWithFont:[label font]];
+    
+    frame.size = size;
+    frame.size.height = 18.0;
+    frame.origin.x = cellSize.width - (frame.size.width + kXPadding);
+    frame.origin.y = 23.0;
+    
+    [label setFrame:frame];
+    [label setContentMode:UIViewContentModeRedraw];
+
+    // commentsView has height (as needed) and width (as needed) and anchors at (cellWidth - (commentsViewWidth + padding), padding)
+    label = self.commentsView;
+    size = [[label text] sizeWithFont:[label font]];
+
+    frame.size = size;
+    frame.size.height = 18.0;
+    frame.origin.x = cellSize.width - (frame.size.width + kXPadding);
+    frame.origin.y = 4.0;
+    
+    [label setFrame:frame];
+    [label setContentMode:UIViewContentModeRedraw];
+    
+    // uploaderView has height (as needed) and width (cellWidth - xAnchor - votesViewWidth - padding) and anchors at (thumbnailViewRightEdge + padding, cellHeight - (uploaderViewHeight + padding)
+    label = self.uploaderView;
+    size = [[label text] sizeWithFont:[label font]];
+
+    frame.origin.x = thumbFrame.origin.x + thumbFrame.size.width + kXPadding;
+    frame.origin.y = 23.0;
+    frame.size = size;
+    frame.size.height = 18.0;
+    frame.size.width = MIN(cellSize.width - (frame.origin.x + [self.votesView frame].size.width + kXPadding), frame.size.width);
+    
+    [label setFrame:frame];
+    [label setContentMode:UIViewContentModeRedraw];
+    
+    // filenameView has height (as needed) and width (xAnchor - commentsViewXAnchor - padding) and anchors at (thumbnailViewWidth + padding, padding)
+    label = self.filenameView;
+    size = [[label text] sizeWithFont:[label font]];
+
+    frame.origin.x = thumbFrame.origin.x + thumbFrame.size.width + kXPadding;
+    frame.origin.y = 3.0;
+    frame.size = size;
+    frame.size.height = 20.0;
+    frame.size.width = MIN(cellSize.width - (frame.origin.x + [self.commentsView frame].size.width + 5.0), frame.size.width);
+    
+    [label setFrame:frame];
+    [label setContentMode:UIViewContentModeRedraw];
 }
 
 @end
