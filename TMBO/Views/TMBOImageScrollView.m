3d19c823ae1a51acf76a6e5aefdda2111a45dda9
Scott Perry <github@numist.net>
2012-09-24 16:52:33 -0700

    Simplify image centering code.
    That shit looked like it came from Stack Overflow, and I couldn't find
    where I got it for attribution, so out it goes!

--- TMBO/Views/TMBOImageScrollView.m
+++ TMBO/Views/TMBOImageScrollView.m
@@ -20,23 +20,14 @@
 - (void)layoutSubviews {
     [super layoutSubviews];
     
-    // center the image as it becomes smaller than the size of the screen
     CGSize bounds = self.bounds.size;
     CGRect frame = imageView.frame;
     
-    // center horizontally
-    if (frame.size.width < bounds.width)
-        frame.origin.x = (bounds.width - frame.size.width) / 2;
-    else
-        frame.origin.x = 0;
-    
-    // center vertically
-    if (frame.size.height < bounds.height)
-        frame.origin.y = (bounds.height - frame.size.height) / 2;
-    else
-        frame.origin.y = 0;
-    
-    imageView.frame = frame;
+    // center the image as it becomes smaller than the size of the screen. If it is larger, set the origin to (0,0).
+    frame.origin.x = MAX(((bounds.width - frame.size.width) / 2), 0);
+    frame.origin.y = MAX(((bounds.height - frame.size.height) / 2), 0);
+
+    [imageView setFrame:frame];
 }
 
 @end
